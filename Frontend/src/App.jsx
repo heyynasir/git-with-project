970ed10b79476393d7c5f4f5e2a1079145100061
Nasir <heyynasir@gmail.com>
2026-03-27 15:29:38 +0530

    create a Home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
 
 function App() {
   return (
     <div className=''>
       <Navbar />
+
+      <Routes>
+          <Route path='/' element={<Home />} />
+      </Routes>
     </div>
   )
 }
---
+++ Frontend/src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='w-full h-[90vh] flex justify-center items-center text-3xl'>Home</div>
+  )
+}
+
+export default Home
