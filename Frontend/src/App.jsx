793efbdf8fa9a0e385151c4f84092125bc095fc2
Nasir <heyynasir@gmail.com>
2026-03-27 15:33:13 +0530

    added about page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import About from './pages/About'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
 
       <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
       </Routes>
     </div>
   )
---
+++ Frontend/src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='w-full h-[90vh] flex justify-center items-center text-3xl'>About</div>
+  )
+}
+
+export default About
